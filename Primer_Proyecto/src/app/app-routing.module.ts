9e566002acff2f5fe5873b0b59fbf03deb4860c8
guillermomarin21 <101566923+guillermomarin21@users.noreply.github.com>
2022-09-14 09:37:17 -0300

    login/registro

--- Primer_Proyecto/src/app/app-routing.module.ts
+++ Primer_Proyecto/src/app/app-routing.module.ts
@@ -11,14 +11,7 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'registro',
-    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
-  },
+  
   {
     path: 'forgot-password',
     loadChildren: () => import('./forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
